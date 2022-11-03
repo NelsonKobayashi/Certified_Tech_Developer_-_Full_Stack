49a983bed7f29ce30fe59e70626bce86a6557feb
Nelson Kobayashi <43545285+NelsonKobayashi@users.noreply.github.com>
2022-11-03 00:11:06 -0300

    Delete 3º Bimestre/Aula5-Proxy directory

--- 3º Bimestre/Aula5-Proxy/src/com/vacina/IRegistro.java
+++
@@ -1,6 +0,0 @@
-package com.vacina;
-
-//Interface que gera o metodo registrar, passando como parâmetros os dados de registro da vacina.
-public interface IRegistro {
-    void registrar(Object[] dados);
-}
