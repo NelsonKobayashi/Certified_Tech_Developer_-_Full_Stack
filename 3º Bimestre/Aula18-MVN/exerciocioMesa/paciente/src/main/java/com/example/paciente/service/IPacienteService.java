41f82b2b9686ff53bc4f8e14a0e9ca4b37742fa2
Nelson Kobayashi <43545285+NelsonKobayashi@users.noreply.github.com>
2022-11-03 14:56:55 -0300

    went up to SpringBoot and MVN classes

---
+++ 3º Bimestre/Aula18-MVN/exerciocioMesa/paciente/src/main/java/com/example/paciente/service/IPacienteService.java
@@ -0,0 +1,10 @@
+package com.example.paciente.service;
+
+import com.example.paciente.domain.Paciente;
+
+import java.util.List;
+
+public interface IPacienteService {
+
+    List<Paciente> listPaciente();
+}
