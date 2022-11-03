f80b4c669d523556d92a41c40b6dc0efabe99abf
Nelson Kobayashi <43545285+NelsonKobayashi@users.noreply.github.com>
2022-11-03 00:10:56 -0300

    Delete 3º Bimestre/Aula4-ChainsOfResponsibility directory

--- 3º Bimestre/Aula4-ChainsOfResponsibility/ExercicoMesaQualidadeDoProduto/src/HandlerAceito.java
+++
@@ -1,6 +0,0 @@
-public class HandlerAceito extends Handler{
-    @Override
-    public void verificar(Artigo artigo) {
-        System.out.println("O produto foi ACEITO nos parâmetros de qualidade exigidos!");
-    }
-}
